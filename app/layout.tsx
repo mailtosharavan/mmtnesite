745949ae5fd8dd1967d3292418ade2701c33e299
hriday <hridayway@gmail.com>
2025-12-26 12:32:54 +0530

    Tag manager code update

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,14 +60,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PWH4T6JN');`,
+            })(window,document,'script','dataLayer','GTM-5CDC9LGF');`,
           }}
         />
       </head>
       <body suppressHydrationWarning>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PWH4T6JN"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5CDC9LGF"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
